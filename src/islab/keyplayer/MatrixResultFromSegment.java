fe0b8fb2626fa0c40a86524ca9475414aa6e103b
Duy Tri <duytri1009@gmail.com>
2016-08-01 11:01:36 +0700

    Try To Improve Memory Allocation

--- src/islab/keyplayer/MatrixResultFromSegment.java
+++ src/islab/keyplayer/MatrixResultFromSegment.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class MatrixResultFromSegment extends Thread {
 	private Map<String[], BigDecimal> mapResult = new HashMap<String[], BigDecimal>();
@@ -21,15 +22,22 @@ public class MatrixResultFromSegment extends Thread {
 		// TODO Auto-generated method stub
 		try{
 			for (Segment segment : listSegment) {
-				String[] coor = { segment.getStartVertex(), segment.getEndVertex() };
-				BigDecimal bdTemp = mapResult.get(coor);
+				String[] coor = null; //{ segment.getStartVertex(), segment.getEndVertex() };
+				BigDecimal bdTemp = null; //mapResult.get(coor);
+				for (Entry<String[], BigDecimal> entry : mapResult.entrySet()) {
+					coor = entry.getKey();
+					if (coor[0].equals(segment.getStartVertex()) && coor[1].equals(segment.getEndVertex())){
+						bdTemp = entry.getValue();
+						break;
+					}
+				}
 				if (bdTemp != null) {
 					bdTemp = bdTemp.multiply(BigDecimal.ONE.subtract(segment.getIndirectInfluence()));
 					mapResult.replace(coor, bdTemp);
 				}
 				else {
 					bdTemp = BigDecimal.ONE.subtract(segment.getIndirectInfluence());
-					mapResult.putIfAbsent(coor, bdTemp);
+					mapResult.putIfAbsent(new String[]{segment.getStartVertex(), segment.getEndVertex()}, bdTemp);
 				}
 			}
 		}
